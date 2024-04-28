9575492539969de33a1dc1b945f3ef99d0765a2c
Cristian <cristian2306c@gmail.com>
2024-04-28 16:46:47 +0200

    Completados los servicios para verificar si una habitación ha sido limpiada hoy y obtener las habitaciones que han sido limpiadas hoy

--- src/limpieza/limpieza.controller.ts
+++ src/limpieza/limpieza.controller.ts
@@ -5,34 +5,39 @@ import { UpdateLimpiezaDto } from './dto/update-limpieza.dto';
 
 @Controller('limpieza')
 export class LimpiezaController {
+  // Inyección de dependencias del servicio de limpieza
   constructor(private readonly limpiezaService: LimpiezaService) {}
 
-  @Get(':id')
-  findAll(@Param('id') id: string) {
-    return this.limpiezaService.findAll(id);
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  create(@Body() createLimpiezaDto: CreateLimpiezaDto) {
-    return this.limpiezaService.create(createLimpiezaDto);
-  }
-
-  @Put(':id')
-  @UsePipes(ValidationPipe)
-  update(@Param('id') id: string, @Body() updateLimpiezaDto: UpdateLimpiezaDto) {
-    return this.limpiezaService.update(id, updateLimpiezaDto);
-  }
-
-  @Get('limpia/:id')
-  limpiezaHoy(@Param('id') id: string) {
-    return this.limpiezaService.limpiezaHoy(id);
-  }
-
+  // Ruta GET para obtener las habitaciones limpias hoy
   @Get('limpias')
   habitacionesLimpiasHoy(){
     return this.limpiezaService.habitacionesLimpiasHoy();
   }
 
+  // Ruta GET para obtener todas las limpiezas de una habitación específica
+  @Get(':id')
+  findAll(@Param('id') id: string) {
+    return this.limpiezaService.findAll(id);
+  }
 
+  // Ruta POST para crear una nueva limpieza
+  @Post()
+  @UsePipes(ValidationPipe) // Uso de un pipe de validación para validar los datos de entrada
+  create(@Body() createLimpiezaDto: CreateLimpiezaDto) {
+    return this.limpiezaService.create(createLimpiezaDto);
+  }
+
+  // Ruta PUT para actualizar una limpieza existente
+  @Put(':id')
+  @UsePipes(ValidationPipe) // Uso de un pipe de validación para validar los datos de entrada
+  update(@Param('id') id: string, @Body() updateLimpiezaDto: UpdateLimpiezaDto) {
+    return this.limpiezaService.update(id, updateLimpiezaDto);
+  }
+
+  // Ruta GET para verificar si una habitación ha sido limpiada hoy
+  @Get('limpia/:id')
+  limpiezaHoy(@Param('id') id: string) {
+    return this.limpiezaService.limpiezaHoy(id);
+  }
 }
+
